ddd1faa2738ec38b1d0a3ee0d11ea4b7e1d315bc
Naoufal <bidari.naoufal@gmail.com>
2021-08-06 13:06:58 +0100

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const {Router} = require('express');
 const multer = require('multer');
+const imageProcessor = require('./imageProcessor');
 
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
@@ -27,9 +28,14 @@ const upload = multer({
   storage,
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) {
     return response.status(400).json({error: request.fileValidationError});
+  }
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {
+    //
   }
   return response.status(201).json({success: true});
 });
